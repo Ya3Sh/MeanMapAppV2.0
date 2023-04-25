ae15410e540ce2575a9e5547bf79414fbfd765bf
Yateen Shaha <yateenshaha@gmail.com>
2023-04-25 14:38:20 -0400

    fix json

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
         name: "MongoDB Service",
         url: "mongodb://" + process.env.DATABASE_USER + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
         port: process.env.DATABASE_PORT
-    }
+    },
     
     mongolab:
     {
